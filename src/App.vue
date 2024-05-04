db95de982bc28b2c7d35b56bba49712a3c6412ca
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-05-04 10:19:56 -0300

    created the form component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <main>
-    
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">
+      <Form />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue';
+import Form from './components/Form.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    SideBar,
+    Form
+  }
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
