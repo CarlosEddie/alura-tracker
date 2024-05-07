f18e57baa88b3e5746e1102fcf81d6fcf33f384d
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-05-07 10:39:24 -0300

    created the task component

---
+++ src/components/Task.vue
@@ -0,0 +1,34 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ task.description || 'Task without description'}}
+            </div>
+            <div class="column">
+                <Stopwatch :time-in-seconds="task.durationInSeconds" />
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import ITask from '@/interfaces/ITask';
+import Stopwatch from './Stopwatch.vue';
+import { defineComponent, PropType } from 'vue';
+import Box from './Box.vue';
+
+export default defineComponent({
+    name: 'TaskComponent',
+    components: {
+        Stopwatch,
+        Box,
+    },
+    props: {
+        task: {
+            type: Object as PropType<ITask>,
+            required: true
+        }
+    }
+})
+
+</script>
