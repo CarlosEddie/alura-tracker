a3c4e5741ef720143539f9f8f40a48ada47420e2
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-05-14 10:46:07 -0300

    created the projects route

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,38 +3,56 @@
         <h1>
             <img src="../assets/logo.png" alt="">
         </h1>
-        <button class="button" @click="changeTheme">
-            {{ buttonText }}
-        </button>
+        <div class="has-text-centered">
+            <button class="button" @click="changeTheme">
+                {{ buttonText }}
+            </button>
+        </div>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <RouterLink to="/" class="link">
+                        <i class="fas fa-tasks"></i>
+                        tasks
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/projects" class="link">
+                        <i class="fas fa-project-diagram"></i>
+                        projects
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-    export default defineComponent({
-        name: 'SideBar',
-        emits: ['whenThemeChanged'],
-        data() {
-            return {
-                activeDarkMode: false,
-            }
-        },
-        computed: {
-            buttonText() {
-                if(this.activeDarkMode) {
-                    return 'Disable dark mode'
-                }
-                return 'Enable dark mode'
-            }
-        },
-        methods: {
-            changeTheme () {
-                this.activeDarkMode = !this.activeDarkMode
-                this.$emit('whenThemeChanged', this.activeDarkMode)
-            }
+export default defineComponent({
+    name: 'SideBar',
+    emits: ['whenThemeChanged'],
+    data() {
+        return {
+            activeDarkMode: false,
         }
-    })
+    },
+    computed: {
+        buttonText() {
+            if (this.activeDarkMode) {
+                return 'Disable dark mode'
+            }
+            return 'Enable dark mode'
+        }
+    },
+    methods: {
+        changeTheme() {
+            this.activeDarkMode = !this.activeDarkMode
+            this.$emit('whenThemeChanged', this.activeDarkMode)
+        }
+    }
+})
 </script>
 
 <style scoped>
@@ -51,5 +69,21 @@ header {
         padding: 2.5rem;
         height: auto;
     }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+
+.link {
+    color: #fff;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
