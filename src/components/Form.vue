f7c9894e36c42b1fb950033d8f56f92eabbccedb
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-05-17 11:27:32 -0300

    created the state to control the projects

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,19 +1,31 @@
 <template>
     <div class="box form">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Form for creating a new task">
-                <input type="text" class="input" placeholder="What task do you want to start?" v-model="description">
+            <div class="column is-5" role="form" aria-label="Form for creating a new task">
+                <input type="text" class="input" placeholder="What task do yo   u want to start?" v-model="description">
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="projectId">
+                        <option value="">Select a project</option>
+                        <option :value="project.id" v-for="project in projects" :key="project.id">
+                            {{ project.name }}
+                        </option>
+                    </select>
+                </div>
             </div>
             <div class="column">
-                <Timer @when-timer-finished="finishTask"/>
+                <Timer @when-timer-finished="finishTask" />
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Timer from './Timer.vue';
+import { useStore } from 'vuex';
+import { key } from '@/store';
 
 export default defineComponent({
     name: 'FormComponent',
@@ -23,16 +35,24 @@ export default defineComponent({
     },
     data() {
         return {
-            description: ''
+            description: '',
+            projectId: ''
         }
     },
     methods: {
-        finishTask(elapsedTime: number) : void {
+        finishTask(elapsedTime: number): void {
             this.$emit('whenSaveTask', {
                 durationInSeconds: elapsedTime,
-                description: this.description
+                description: this.description,
+                project: this.projects.find(project => project.id == this.projectId)
             })
             this.description = ''
+        }
+    },
+    setup() {
+        const store = useStore(key)
+        return {
+            projects: computed(() => store.state.projects)
         }
     }
 })
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,7 +4,7 @@
         <form @submit.prevent="save">
             <div class="field">
                 <label for="projectName" class="label">Project Name</label>
-                <input type="text" class="input" v-model="projectName" id="projectName"/>
+                <input type="text" class="input" v-model="projectName" id="projectName" />
             </div>
             <div class="field">
                 <button class="button" type="submit">Save</button>
@@ -29,27 +29,30 @@
 
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    import IProject from '../interfaces/IProject';
-    export default defineComponent({
-        name: 'ProjectsComponent',
-        data() {
-            return {
-                projectName: "",
-                projects: [] as IProject[]
-            }
-        },
-        methods: {
-            save() {
-                const project: IProject = {
-                    name: this.projectName,
-                    id: new Date().toISOString()
-                }
-                this.projects.push(project)
-                this.projectName = ''
-            }
+import { useStore } from '@/store'
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProjectsComponent',
+    data() {
+        return {
+            projectName: "",
         }
-    })
+    },
+    methods: {
+        save() {
+            this.store.commit('ADD_PROJECT', this.projectName)
+            this.projectName = ''
+        }
+    },
+    setup() {
+        const store = useStore()
+        return {
+            store,
+            projects: computed(() => store.state.projects)
+        }
+    }
+})
 </script>
 
 <style scoped>
