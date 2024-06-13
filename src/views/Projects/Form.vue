ff2f568cffe715bc121ff2122b41bff348047016
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-06-13 10:02:30 -0300

    moved the save and dealWithSuccess methods to setup

--- src/views/Projects/Form.vue
+++ src/views/Projects/Form.vue
@@ -19,6 +19,7 @@ import { useStore } from '@/store'
 import { defineComponent, ref } from 'vue';
 import useNotifier from '@/hooks/notifier'
 import { CHANGE_PROJECT, REGISTER_PROJECT } from '@/store/actions-type';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
     name: 'FormComponent',
@@ -27,41 +28,41 @@ export default defineComponent({
             type: String
         }
     },
-    methods: {
-        save() {
-            if (this.id) {
-                this.store.dispatch(CHANGE_PROJECT, { id: this.id, name: this.projectName })
-                    .then(() => {
-                        this.dealWithSuccess()
-                    })
-            } else {
-                this.store.dispatch(REGISTER_PROJECT, this.projectName)
-                    .then(() => {
-                        this.dealWithSuccess()
-                    })
-            }
-
-        },
-        dealWithSuccess() {
-            this.projectName = ''
-            this.notify(NotificationType.SUCCESS, 'Excellent', 'Alright, your project has been saved.')
-            this.$router.push('/projects')
-        }
-    },
     setup(props) {
+        const router = useRouter()
         const store = useStore()
         const { notify } = useNotifier()
         const projectName = ref("")
 
-        if(props.id) {
+        if (props.id) {
             const project = store.state.project.projects.find(project => project.id == props.id)
             projectName.value = project?.name || ''
         }
+
+        const dealWithSuccess = () => {
+            projectName.value = ''
+            notify(NotificationType.SUCCESS, 'Excellent', 'Alright, your project has been saved.')
+            router.push('/projects')
+        }
+
+        const save = () => {
+            if (props.id) {
+                store.dispatch(CHANGE_PROJECT, { id: props.id, name: projectName.value })
+                    .then(() => {
+                        dealWithSuccess()
+                    })
+            } else {
+                store.dispatch(REGISTER_PROJECT, projectName.value)
+                    .then(() => {
+                        dealWithSuccess()
+                    })
+            }
+
+        }
         return {
-            store,
-            notify,
             projectName,
+            save,
         }
     }
 })
-</script>
+</script>「OooDa・土屋李央 今日は何時から？」第2回アーカイヴ配信
